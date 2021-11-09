beddda43c4d96380a89e7a22d093e301a0668a0d
Nikesh Maharjan <nikeshmhr@gmail.com>
2021-11-09 21:22:42 +0545

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
-  },
-  plugins: ["gatsby-plugin-sass"],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "My Gatsby Site",
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+	],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
@@ -8,6 +9,10 @@ const IndexPage = () => {
 			<h1>Welcome to my Gatsby site!</h1>
 			<Link to="/about">About</Link>
 			<p>I'm making this by following the Gatsby Tutorial</p>
+			<StaticImage
+				alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+				src="../images/typewriter.jpg"
+			/>
 		</Layout>
 	);
 };
